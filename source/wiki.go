f1a82f9b111d5a14e6a052f6494324e987ad18f0
Benton Drew <benton.s.drew@drewantech.com>
2018-09-06 12:19:32 -0500

    Implement function literals and closures from tutorial.

--- source/wiki.go
+++ source/wiki.go
@@ -29,6 +29,17 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
   return m[2], nil
 }
 
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
+    }
+    fn(w, r, m[2])
+  }
+}
+
 func (p *Page) save() error {
   filename := p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
@@ -51,11 +62,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
   }
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/" + title, http.StatusFound)
@@ -64,11 +71,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     p = &Page{Title: title}
@@ -76,24 +79,20 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
   err := p.save()
   if err != nil {
-    http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
   http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 func main() {
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  http.HandleFunc("/save/", saveHandler)
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
